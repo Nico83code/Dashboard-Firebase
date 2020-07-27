2184a5b4f80ab8d9085b4d21a6846a58d3890940
nicoman@gmail.com <nico.man.codaisseur@gmail.com>
2020-07-27 22:51:16 +0200

    bugfixed

---
+++ dist/precache-manifest.3b3b802d8af728b68c570a6248724232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1d80f5437ed44afcfebfaf561f7033",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e465f804514ecbd7f5c",
+    "url": "/static/css/main.cc4e9e1d.chunk.css"
+  },
+  {
+    "revision": "6d8eaf058baf2ea7e92c",
+    "url": "/static/js/2.7863abe1.chunk.js"
+  },
+  {
+    "revision": "29b76bdf57b2975698374584ae6ceb16",
+    "url": "/static/js/2.7863abe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e465f804514ecbd7f5c",
+    "url": "/static/js/main.b4de9c53.chunk.js"
+  },
+  {
+    "revision": "dbe2df52b783bef4d891",
+    "url": "/static/js/runtime-main.60b6ba70.js"
+  }
+]);
